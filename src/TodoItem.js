b9bf4b76e8e9d3b08ead8a808b3d31488e2957ee
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-05-27 11:57:10 -0500

    se agrego diseño final

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,7 +4,7 @@ function TodoItem(props) {
     return (
       <li className="TodoItem">
         <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>v</span>
-        <p className={`TodoItem-p ${props.completed &&"TodoItem-p--complete"}`}>{props.text}</p>
+        <p className={`TodoItem-p ${props.completed &&"TodoItem-p--completed"}`}>{props.text}</p>
         <span className="Icon Icon-delete">X</span>
       </li>
     );
