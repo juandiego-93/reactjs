d3d8a73fff9c77c49b5e7cf3f4c2428e13812ee2
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-05-27 16:48:40 -0500

    +1

--- src/ToDoSearch.js
+++ src/ToDoSearch.js
@@ -1,18 +1,26 @@
+import React, { useState } from 'react';
 import './TodoSearch.css'
 
 function ToDoSearch() {
-    return (
-      <input 
-        placeholder="Cortar cebolla"
-        className="TodoSearch"
-        onChange={(event)=>{
-          console.log('Escribiste en el evento')
-          // console.log(event);
-          // console.log(event.target);
-          console.log(event.target.value);
-        }}
-      />
-    );
-  }
+  const [searchValue, setSearchValue] = React.useState();
+  useState('')
+  
+  console.log('los usuarios buscan todos de ' + searchValue)
 
-export  { ToDoSearch };
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+        // console.log('Escribiste en el evento')
+        // console.log(event);
+        // console.log(event.target);
+        // console.log(event.target.value);
+      }}
+    />
+  );
+}
+
+export { ToDoSearch };
