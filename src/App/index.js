e5e79aef53bb91c0efce856c83de9faf7fec029a
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-07 22:20:01 -0500

    Clase 5 y 6

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { AppUI } from './AppUI';
 import { TodoProvider } from '../TodoContext';
 
-
 function App() {
   // console.log('los usuarios buscan todos de ' + searchValue)
 
