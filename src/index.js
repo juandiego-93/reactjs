5053c064ab7eba66c3131577d270c66eeb4dcb6f
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-10 10:50:21 -0500

    HOC- clase ejemplo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+// import App from './App';
 import './index.css';
-import App from './App';
+
+function App(props) {
+    return(
+        <h1> {props.greeting}, {props.name}! </h1>
+    )
+}
+
+function withSaludo(Wrappedcomponent) {
+    return function  WrappedcomponentWithSaludo(saludo) {
+        return function ComponenteDeVerdad(props) {
+            return (
+                <>
+                    <Wrappedcomponent {...props} greeting={saludo} />
+                    <p>Estamos acompañando al WrappedComponent</p>
+                </>
+            )
+        }
+        
+    }
+}
+
+const AppWithSaludo = withSaludo(App)('Wenas')
 
 const root = document.getElementById('root');
-render(<App />, root);
+ReactDOM.render(
+    <AppWithSaludo name='JD'/>,
+    root
+);
